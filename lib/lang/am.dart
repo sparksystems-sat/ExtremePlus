5e61d1b196447f988f6a0e373ef867cd6171dd03
TeshagerAndarge <teshandarge@gmail.com>
2025-07-25 23:15:16 +0300

    first comment

---
+++ lib/lang/am.dart
@@ -0,0 +1,7 @@
+class AppTextAm {
+  static const String appName = "የፈተና ማስተላለፊያ";
+  static const String welcomeMessage = "እንኳን ወደ የፈተና ማስተላለፊያ መተግበሪያ በደህና መጡ";
+  static const String startExam = "ፈተና ጀምር";
+  static const String viewResults = "ውጤቶችን እንዲመልከቱ";
+  static const String settings = "ቅንብሮች";
+}
---
+++ lib/main.dart
@@ -0,0 +1,68 @@
+import 'package:exam_practice_app/lang/en.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+void main() {
+
+  MultiBlocProvider(
+    providers: [
+      // Add your Bloc providers here
+    ],
+    child: const MyApp(),
+  );
+
+  runApp(const MyApp());
+}
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: AppTextEn.appName,
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const MyHomePage(title: AppTextEn.welcomeMessage),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: GridView.count(
+        crossAxisCount: 3, // 3 columns
+        crossAxisSpacing: 10.0,
+        mainAxisSpacing: 10.0,
+        padding: const EdgeInsets.all(10.0),
+        children: List.generate(
+          20, // Generate 20 items
+          (index) {
+            return Container(
+              color: Colors.blueAccent[100 * (index % 9 + 1)],
+              child: Center(
+                child: Text(
+                  'Item $index',
+                  style: const TextStyle(color: Colors.white, fontSize: 18),
+                ),
+              ),
+            );
+          },
+        ),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
