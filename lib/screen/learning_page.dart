067e739d923b38e9043082a61495f95d8272432d
Teshager Andargie <teshandarge@gmail.com>
2025-10-17 18:04:28 +0300

    learning page with tab view

--- lib/screen/learning_page.dart
+++ lib/screen/learning_page.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_pdfviewer/pdfviewer.dart';
 
 class LearningPage extends StatefulWidget {
-    final int subject_id;
-  const LearningPage({super.key ,required this.subject_id});
+  final int subject_id;
+  const LearningPage({super.key, required this.subject_id});
 
   @override
   State<LearningPage> createState() => _LearningPageState();
@@ -15,34 +16,36 @@ class _LearningPageState extends State<LearningPage> {
       length: 3,
       child: Scaffold(
         appBar: AppBar(
+          elevation: 0,
           title: Text("GeeksForGeeks"),
           bottom: TabBar(
             tabs: [
-              Tab(
-                icon: Icon(Icons.home_filled),
-                text: "Home",
-              ),
-              Tab(
-                icon: Icon(Icons.account_box_outlined),
-                text: "Account",
-              ),
-              Tab(
-                icon: Icon(Icons.alarm),
-                text: "Alarm",
-              ),
+              Tab(icon: Icon(Icons.home_filled), text: "Home"),
+              Tab(icon: Icon(Icons.account_box_outlined), text: "Account"),
+              Tab(icon: Icon(Icons.alarm), text: "Alarm")
+             
             ],
           ),
         ),
         body: TabBarView(
           children: [
-            Center(
-              child: Icon(Icons.home),
+           Container(
+              child: SfPdfViewer.network(
+                'https://cdn.syncfusion.com/content/PDFViewer/encrypted.pdf',
+                password: 'syncfusion',
+              ),
             ),
-            Center(
-              child: Icon(Icons.account_circle),
+            Container(
+              child: SfPdfViewer.network(
+                'https://cdn.syncfusion.com/content/PDFViewer/encrypted.pdf',
+                password: 'syncfusion',
+              ),
             ),
-            Center(
-              child: Icon(Icons.alarm),
+            Container(
+              child: SfPdfViewer.network(
+                'https://cdn.syncfusion.com/content/PDFViewer/encrypted.pdf',
+                password: 'syncfusion',
+              ),
             )
           ],
         ),
